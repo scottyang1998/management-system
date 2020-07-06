9f7f68c4dfed0d0c48a5a710fbcd412223b1304d
scottyang <395670149@qq.com>
2020-07-06 23:28:23 +0800

    验证表单和按钮

--- 课程设计-3117005001-杨道潮/财务管理系统/client1/src/views/Register.vue
+++ 课程设计-3117005001-杨道潮/财务管理系统/client1/src/views/Register.vue
@@ -2,7 +2,7 @@
     <div class="register">
         <section class="form_container">
             <div class="manage_tip">
-                <span class="title">米修在线后台管理系统</span>
+                <span class="title">财务后台管理系统</span>
             </div>
             <el-form :model="registerUser" :rules="rules" class="registerForm" ref="registerForm" label-width="80px">
                 <el-form-item label="用户名" prop="name">
@@ -33,112 +33,112 @@
 
 <script>
 export default {
-  name: "register",
-  data() {
-    var validatePass2 = (rule, value, callback) => {
-      if (value !== this.registerUser.password) {
-        callback(new Error("两次输入密码不一致!"));
-      } else {
-        callback();
-      }
+    name: "register",
+    data() {
+        var validatePass2 = (rule, value, callback) => {
+        if (value !== this.registerUser.password) {
+            callback(new Error("两次输入密码不一致!"));
+        } else {
+            callback();
+        }
     };
     return {
-      registerUser: {
+        registerUser: {
         name: "",
         email: "",
         password: "",
         password2: "",
         identity: ""
-      },
-      rules: {
+        },
+        rules: {
         name: [
-          { required: true, message: "用户名不能为空", trigger: "change" },
-          { min: 2, max: 30, message: "长度在 2 到 30 个字符", trigger: "blur" }
+            { required: true, message: "用户名不能为空", trigger: "change" },
+            { min: 2, max: 30, message: "长度在 2 到 30 个字符", trigger: "blur" }
         ],
         email: [
-          {
+        {
             type: "email",
             required: true,
             message: "邮箱格式不正确",
             trigger: "blur"
-          }
+        }
         ],
         password: [
-          { required: true, message: "密码不能为空", trigger: "blur" },
-          { min: 6, max: 30, message: "长度在 6 到 30 个字符", trigger: "blur" }
+        { required: true, message: "密码不能为空", trigger: "blur" },
+        { min: 6, max: 30, message: "长度在 6 到 30 个字符", trigger: "blur" }
         ],
         password2: [
-          { required: true, message: "确认密码不能为空", trigger: "blur" },
-          {
+        { required: true, message: "确认密码不能为空", trigger: "blur" },
+        {
             min: 6,
             max: 30,
             message: "长度在 6 到 30 个字符",
             trigger: "blur"
-          },
-          { validator: validatePass2, trigger: "blur" }
+        },
+        { validator: validatePass2, trigger: "blur" }
         ]
-      }
-    };
-  },
-  methods: {
+        }
+        };
+    },
+    methods: {
     submitForm(formName) {
-      this.$refs[formName].validate(valid => {
+        this.$refs[formName].validate(valid => {
         if (valid) {
-          this.$axios
+            this.$axios
             .post("/api/users/register", this.registerUser)
             .then(res => {
               // 注册成功
-              this.$message({
+                this.$message({
                 message: "注册成功！",
                 type: "success"
-              });
+                });
               // this.$router.push("/login");
             });
         } else {
-          console.log("error submit!!");
-          return false;
+            console.log("error submit!!");
+            return false;
         }
-      });
+        });
     }
-  }
+    }
 };
 </script>
 
 
 <style scoped>
 .register {
-  position: relative;
-  width: 100%;
-  height: 100%;
-  background: url(../assets/bg.jpg) no-repeat center center;
-  background-size: 100% 100%;
+    position: relative;
+    width: 100%;
+    height: 100%;
+    background: url(../assets/bg.jpg) no-repeat center center;
+    background-size: 100% 100%;
 }
 .form_container {
-  width: 370px;
-  height: 210px;
-  position: absolute;
-  top: 10%;
-  left: 34%;
-  padding: 25px;
-  border-radius: 5px;
-  text-align: center;
+    width: 370px;
+    height: 210px;
+    position: absolute;
+    top: 10%;
+    left: 34%;
+    padding: 25px;
+    border-radius: 5px;
+    text-align: center;
 }
 .form_container .manage_tip .title {
-  font-family: "Microsoft YaHei";
-  font-weight: bold;
-  font-size: 26px;
-  color: #fff;
+    font-family: "Microsoft YaHei";
+    font-weight: bold;
+    font-size: 26px;
+    color: #fff;
 }
 .registerForm {
-  margin-top: 20px;
-  background-color: #fff;
-  padding: 20px 40px 20px 20px;
-  border-radius: 5px;
-  box-shadow: 0px 5px 10px #cccc;
+    margin-top: 20px;
+    background-color: #fff;
+    padding: 20px 40px 20px 20px;
+    border-radius: 5px;
+    box-shadow: 0px 5px 10px #cccc;
 }
 
 .submit_btn {
-  width: 100%;
+    width: 100%;
 }
 </style>
 
