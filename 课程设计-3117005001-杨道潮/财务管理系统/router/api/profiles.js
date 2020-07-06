001105eec787b64b169f527c26f11254310cace7
scottyang <395670149@qq.com>
2020-07-06 12:54:04 +0800

    编辑信息 删除信息接口

--- 课程设计-3117005001-杨道潮/财务管理系统/router/api/profiles.js
+++ 课程设计-3117005001-杨道潮/财务管理系统/router/api/profiles.js
@@ -63,6 +63,57 @@ router.get(
 
 
 
+// //$router POST api/profiles/edit
+// //@desc  编辑信息接口
+// //@access private
+router.post(
+    "/edit/:id",
+    passport.authenticate('jwt',{session:false}),(req,res)=>{
+    const profileFields = {};
+
+    if(req.body.type) profileFields.type = req.body.type;
+    if(req.body.describe) profileFields.describe = req.body.describe;
+    if(req.body.income) profileFields.income = req.body.income;
+    if(req.body.expend) profileFields.expend = req.body.expend;
+    if(req.body.cash) profileFields.cash = req.body.cash;
+    if(req.body.remark) profileFields.remark = req.body.remark;   
+
+    Profile.findOneAndUpdate(
+        {_id:req.params.id},
+        {$set:profileFields},
+        {new:true}
+    ).then(profile=>res.json(profile))
+})
+
+
+// //$router POST api/profiles/delete/:id
+// //@desc  删除信息接口
+// //@access private
+router.delete(
+    "/delete/:id",
+    passport.authenticate('jwt',{session:false}),(req,res)=>{
+    Profile.findOneAndRemove({_id:req.params.id})
+    .then(profile=>{
+        profile.save().then(profile=>res.json(profile))
+    })
+    .catch(err=>res.status(404).json("删除失败"));
+})
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // router.post("/register",(req,res)=>{
 //     //console.log(req.body);
 //     //查询数据库是否拥有邮箱
@@ -149,4 +200,3 @@ router.get(
 //         identity:req.user.identity
 //     });
 // })
-module.exports = router;
