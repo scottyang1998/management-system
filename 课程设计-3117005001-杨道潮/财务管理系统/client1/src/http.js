da8e7a97c2927db49181224a6e8d976d78720bf9
scottyang <395670149@qq.com>
2020-07-07 17:23:06 +0800

    加载动画 消息提醒（登录）

--- 课程设计-3117005001-杨道潮/财务管理系统/client1/src/http.js
+++ 课程设计-3117005001-杨道潮/财务管理系统/client1/src/http.js
@@ -1,5 +1,5 @@
-import axios from 'axios'
-import { Message,Loading } from 'element-ui';
+import axios from 'axios' 
+import { Message,Loading } from 'element-ui'
 
 
 let loading        //定义loading变量
