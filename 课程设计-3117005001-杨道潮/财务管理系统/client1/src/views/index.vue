4b3050b75995def0d2b1e06a57097904567d1b9c
scottyang <395670149@qq.com>
2020-07-08 09:28:21 +0800

    左侧导航栏

--- 课程设计-3117005001-杨道潮/财务管理系统/client1/src/views/index.vue
+++ 课程设计-3117005001-杨道潮/财务管理系统/client1/src/views/index.vue
@@ -2,16 +2,19 @@
     <div class="index">
 
         <HeadNav></HeadNav>
+        <LeftMenu></LeftMenu>
         <router-view></router-view>
     </div>
 </template>
 
 <script>
     import HeadNav from '../components/HeadNav'
+    import LeftMenu from '../components/LeftMenu'
     export default{
         name: 'index',
         components: {
-            HeadNav
+            HeadNav,
+            LeftMenu
         }
     };
 </script>
